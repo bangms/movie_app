e9e8e8e814db59b90af399ff52bed3d6a9c35204
bangms <uubang@knou.ac.kr>
2020-04-08 12:51:35 +0900

    Updates

---
+++ precache-manifest.50fb553932a051f241a55403844fabc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8001dc5697f9c6794a49a389992d361",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ca052acc66e8b8a20ee5",
+    "url": "/movie_app/static/css/main.7e70d284.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca052acc66e8b8a20ee5",
+    "url": "/movie_app/static/js/main.8007c3f2.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
